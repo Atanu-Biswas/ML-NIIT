74cab8c482984b098e3950d5624868147804c299
venom_007 <78145170+Atanu-Biswas@users.noreply.github.com>
2023-07-22 17:34:13 +0530

    Add files via upload

---
+++ Exr2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+x = np.array([[1,2],[3,4]])
+y = np.array([[9,8,3],[1,6,6]])
+
+print("The element using addition of matrix is: ")
+print(np.add(x,y))
+print("The element using subtraction of matrix is: ")
+print(np.subtract(x,y))
+print("The element using division of matrix is: ")
+print(np.divide(x,y))
